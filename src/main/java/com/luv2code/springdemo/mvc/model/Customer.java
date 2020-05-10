59e757ab998f835aee3a5362eb8360ac31c72589
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-05-10 21:06:45 +0200

    Refactor: freePasses annotated as required (@NotNull) and field definition changed from int to Integer type in order to avoid error during converion empty string from form:input to int primitive type

--- src/main/java/com/luv2code/springdemo/mvc/model/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/model/Customer.java
@@ -13,9 +13,10 @@ public class Customer {
     @Size(min=1, message = "value too short")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value=0, message = "value must be >= 0")
     @Max(value=10, message = "value must be <= 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
